e83ee18216188139418ab5e76baaae52138daedc
tonialtendorf <tvohnoutka05@gmail.com>
2023-01-16 17:02:28 -0600

    added local storage code to js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+
+//$(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +18,23 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+  //});
+
+
+  //day.js
+  var today = dayjs();
+  $('#currentDay').text(today.format('[Today is] MMM D, YYYY'));
+
+  
+  //local storage
+  $("hour-8 .time-block").val(localStorage.getItem("08"));
+  $("hour-9 .time-block").val(localStorage.getItem("09"));
+  $("hour-10 .time-block").val(localStorage.getItem("10"));
+  $("hour-11 .time-block").val(localStorage.getItem("11"));
+  $("hour-12 .time-block").val(localStorage.getItem("12"));
+  $("hour-13 .time-block").val(localStorage.getItem("13"));
+  $("hour-14 .time-block").val(localStorage.getItem("14"));
+  $("hour-15 .time-block").val(localStorage.getItem("15"));
+  $("hour-16 .time-block").val(localStorage.getItem("16"));
+  $("hour-17 .time-block").val(localStorage.getItem("17"));
+
