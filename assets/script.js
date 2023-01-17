bb55908bae6fed5073189805a9ccf446b08bb110
tonialtendorf <tvohnoutka05@gmail.com>
2023-01-16 18:09:52 -0600

    final changes

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@
   //save to local storage
   $(document).ready(function () {
    $('.saveBtn').on('click', function () {
-    var value = $(this).siblings('.description').val();
+    var value = $(this).siblings('.time-block').val();
     var time = $(this).parent().attr('id');
     localStorage.setItem(time, value);
    })
@@ -29,14 +29,14 @@
 
 
   //get from local storage
-  $("hour-8 .description").val(localStorage.getItem("08"));
-  $("hour-9 .description").val(localStorage.getItem("09"));
-  $("hour-10 .description").val(localStorage.getItem("10"));
-  $("hour-11 .description").val(localStorage.getItem("11"));
-  $("hour-12 .description").val(localStorage.getItem("12"));
-  $("hour-13 .description").val(localStorage.getItem("13"));
-  $("hour-14 .description").val(localStorage.getItem("14"));
-  $("hour-15 .description").val(localStorage.getItem("15"));
-  $("hour-16 .description").val(localStorage.getItem("16"));
-  $("hour-17 .description").val(localStorage.getItem("17"));
+  $("hour-8 .time-block").val(localStorage.getItem("hour-8"));
+  $("hour-9 .time-block").val(localStorage.getItem("hour-9"));
+  $("hour-10 .time-block").val(localStorage.getItem("hour-10"));
+  $("hour-11 .time-block").val(localStorage.getItem("hour-11"));
+  $("hour-12 .time-block").val(localStorage.getItem("hour-12"));
+  $("hour-13 .time-block").val(localStorage.getItem("hour-13"));
+  $("hour-14 .time-block").val(localStorage.getItem("hour-14"));
+  $("hour-15 .time-block").val(localStorage.getItem("hour-15"));
+  $("hour-16 .time-block").val(localStorage.getItem("hour-16"));
+  $("hour-17 .time-block").val(localStorage.getItem("hour-17"));
 
